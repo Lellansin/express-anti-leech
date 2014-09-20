d3ae22dfb209f2e91d50133a5099ce9ae2ee9fc6
Lellansin <lellansin@gmail.com>
2014-09-21 00:26:40 +0800

    update to version 0.0.1
    1.optional call
    2.add record function

--- test/express/app.js
+++ test/express/app.js
@@ -1,11 +1,26 @@
 var express = require('express'),
-  http = require('http'),
   path = require('path'),
   app = express();
 
 var AntiLeech = require('../../');
 
-app.use(AntiLeech());
+// white list
+var hosts = ['localhost:3001', 'localhost:8004', 'localhost'];
+
+// filter type
+var exts = ['.png', '.jpg', '.jpeg', '.gif', '.swf', '.flv'];
+
+// default show picture
+var pictrue = "/images/default.png";
+
+app.use(AntiLeech({
+  allow: hosts,
+  exts: exts,
+  log: console.log, // you can use your own
+  default: pictrue
+}));
+
+// keep AntiLeech before use static
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 8004);
 
